d8554f1e35015098bf8a1feb5812ffe2ba5eb7aa
starkjoy <residualcode6@gmail.com>
2023-10-24 10:25:36 +0000

    fixed

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,27 @@
+// js/dashboard_main.js
+import $ from 'jquery';
+import _ from 'lodash';
+
+// Initialize a count variable
+let count = 0;
+
+function updateCounter() {
+  // Increment the count and update the text of the 'count' element
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$(document).ready(function() {
+  // Create and append elements
+  const body = $('body');
+
+  body.append($('<p>').text('Holberton Dashboard'));
+  body.append($('<p>').text('Dashboard data for the students'));
+  const button = $('<button>').text('Click here to get started');
+  body.append(button);
+  body.append($('<p id="count"></p>'));
+  body.append($('<p>').text('Copyright - Holberton School'));
+
+  // Bind the debounce function from Lodash to the click event of the button
+  button.click(_.debounce(updateCounter, 1000));
+});
