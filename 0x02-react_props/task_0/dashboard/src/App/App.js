d8e23ef2f2aeeb450b08f99db438f7f26be53fd9
starkjoy <residualcode6@gmail.com>
2023-10-27 11:19:48 +0000

    task_0 done

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -1,34 +1,24 @@
 import React, { Component } from 'react'
-import logo from '../assets/logo.jpg'
-import { getFullYear, getFooterCopy } from '../utils/utils'
-import './App.css'
+import Notifications from '../Notifications/Notifications'
+import Login from '../Login/Login'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 
 export default class App extends Component {
 	render() {
 		return (
-			<div className="App">
-	  			<header className="App-header">
-		  			<img src={logo} className="App-logo" alt="logo" />
-		  			<h1>School dashboard</h1>
-	  			</header>
-	 			<body className="App-body">
-		 		 	<p>Login to access the full dashboard</p>
-					<label htmlFor="email" onClick={() => {
-						// select corresponding input
-						document.getElementById('password').focus();
-					}}>Email</label>
-					<input type="email" id="email" />
-					<label htmlFor="password" onClick={() => {
-						// select corresponding input
-						document.getElementById('password').focus();
-					}}>Password</label>
-					<input type="password" id="password" />
-					<button>OK</button>
-	  			</body>
-				<footer className="App-footer">
-					<p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-				</footer>
-			</div>
+			<React.Fragment>
+				<Notifications />
+				<div className="App">
+					<Header />
+					<div className="App-body">
+						<Login />
+					</div>
+					<div className="App-footer">
+						<Footer />
+					</div>
+				</div>
+			</React.Fragment>
 		)
 	}
 }
